c0cd4d3511fd70c8e7b7647f1cf6f37760bd5b3e
Michael Grossmann <michael.grossmann@gmail.com>
2012-12-29 21:17:28 -0800

    add ruby interpreter

--- generate_test_data.sh
+++ generate_test_data.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-./generator.rb counter=1,first_name,last_name,street_address,city,state,zip,phone 10000 > Students.table
-./generator.rb counter=1,randfloat=4=2,randint=5=1 10000 > GPA.table
-./generator.rb counter=1,major,bool=Yes=No,bool=Yes=No 10000 > Scholarships.table
+ruby ./generator.rb counter=1,first_name,last_name,street_address,city,state,zip,phone 10000 > Students.table
+ruby ./generator.rb counter=1,randfloat=4=2,randint=5=1 10000 > GPA.table
+ruby ./generator.rb counter=1,major,bool=Yes=No,bool=Yes=No 10000 > Scholarships.table
